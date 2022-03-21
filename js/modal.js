423801a5d231e971ba832f4cb103b31fda4df38d
Aleksandr Sidorenko <Sidor007@yandex.ru>
2022-03-21 16:56:22 +0700

    day 4.1

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,16 @@
-const modalButton = document.querySelectorAll('.more');
-const modal = document.querySelector('.modal');
-const overlay = modal.querySelector('.overlay');
-const closeButton = modal.querySelector('.modal__close');
+const modalButton = document.querySelectorAll(".more");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".overlay");
+const closeButton = modal.querySelector(".modal__close");
+
+const modalHidden = () => {
+  modal.classList.toggle("hidden");
+};
 
 modalButton.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        modal.classList.remove('hidden');
-    });
-})
-
-
-overlay.addEventListener('click', () => {
-    modal.classList.add('hidden');
+  btn.addEventListener("click", modalHidden);
 });
 
-closeButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
-})
+overlay.addEventListener("click", modalHidden);
+
+closeButton.addEventListener("click", modalHidden);
